481ca493dfd899c51a6bed9e1623b93970cbe690
Matthew Upson <matthew.upson@digital.cabinet-office.gov.uk>
2017-05-17 19:48:06 +0100

    Update code_nones query to recognise 'none' class.
    Previously this had assumed that the 'none' classes would have the same
    index. This update runs subqueries to determine the correct code and
    project id to search for.

--- sql/commonqueries/code_nones.sql
+++ sql/commonqueries/code_nones.sql
@@ -6,9 +6,9 @@ and comment_further_comments is Null
 )
 insert into classified (respondent_id, coder_id, code_id, project_code_id,pii,date_coded)
 select respondent_id,
-        2 as coder_id,
-        0 as code_id,
-        0 as project_code_id,
+        (select id from users where username='automated') as coder_id,
+        (select code_id from codes where code='none')  as code_id,
+        (select project_code_id from project_codes where project_code='none') as project_code_id,
         'f' as pii,
         (select * from now()) as date_coded
  from cte;
