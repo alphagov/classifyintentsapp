31f2f24bd647d69e264cf414af76303efdf7e832
Matthew Upson <matthew.upson@digital.cabinet-office.gov.uk>
2017-10-18 10:56:14 +0100

    Fix import order

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from . import views
-
 
 auth = Blueprint('auth', __name__)
+
+from . import views
--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,9 +1,9 @@
 from flask import Blueprint
-from . import views, errors
-from ..models import Permission
-
 
 main = Blueprint('main', __name__)
+
+from . import views, errors
+from ..models import Permission
 
 
 @main.app_context_processor
