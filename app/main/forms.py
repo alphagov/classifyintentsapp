84f715de6578b5256360e1bb72c1862dbdcd865d
Matthew Upson <matthew.upson@digital.cabinet-office.gov.uk>
2017-10-20 00:02:20 +0100

    Remove edit profile page
    * Remove links to edit profile page.
    * Order users on user admin page and hide old users.
    * Delete unecessary templates.

--- app/main/forms.py
+++ app/main/forms.py
@@ -27,9 +27,6 @@ class EditProfileAdminForm(FlaskForm):
                                           'numbers, dots or underscores')])
     confirmed = BooleanField('Confirmed')
     role = SelectField('Role', coerce=int)
-    name = StringField('Real name', validators=[Length(0, 64)])
-    location = StringField('Location', validators=[Length(0, 64)])
-    about_me = TextAreaField('About me')
     submit = SubmitField('Submit')
 
     def __init__(self, user, *args, **kwargs):
